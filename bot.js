3d627abb86410ceebbf181453735759aee3b3fbe
mlibre <m.gh@linuxmail.org>
2023-10-10 20:06:27 +0330

    commit message

---
+++ bot.js
@@ -0,0 +1,157 @@
+const TelegramBot = require( "node-telegram-bot-api" );
+const { exec, which } = require( "shelljs" );
+const fs = require( "fs" );
+const path = require( "path" );
+
+if ( !which( "yt-dlp" ) )
+{
+	console.error( "yt-dlp is not installed. Please install it and try again." );
+	process.exit( 1 );
+}
+
+if ( !which( "instaloader" ) )
+{
+	console.error( "instaloader is not installed. Please install it and try again." );
+	process.exit( 1 );
+}
+
+if ( !which( "aria2c" ) )
+{
+	console.error( "aria2c is not installed. Please install it and try again." );
+	process.exit( 1 );
+}
+
+const token = process.env.TELEGRAM_BOT_TOKEN;
+let allowedUsersArray;
+if ( process.env.ALLOWED_USERS )
+{
+	allowedUsersArray = process.env.ALLOWED_USERS.split( "," );
+}
+
+const bot = new TelegramBot( token, { polling: true });
+async function sendAndDeleteFiles ( chatId, userFolderPath )
+{
+
+	const filesInFolder = fs.readdirSync( userFolderPath );
+
+	if ( filesInFolder.length > 0 )
+	{
+		for ( const file of filesInFolder )
+		{
+			const filePath = path.join( userFolderPath, file );
+			try
+			{
+				await bot.sendDocument( chatId, filePath );
+			}
+			catch ( error )
+			{
+				console.log( error );
+			}
+			finally
+			{
+				fs.unlinkSync( filePath );
+			}
+		}
+	}
+	else
+	{
+		await bot.sendMessage( chatId, "No files to send." );
+	}
+
+}
+
+// bot.on( "message", async ( msg ) =>
+bot.onText( /\/d (.+)/, async ( msg, match ) =>
+{
+	const chatId = msg.chat.id;
+	const { username } = msg.from;
+	const firstname = msg.chat.first_name
+	const [ , link ] = match;
+	const userFolder = username || firstname; // Use username if available, otherwise use firstname
+	const userFolderPath = `./downloads/${userFolder}/`;
+
+	if ( allowedUsersArray && !allowedUsersArray.includes( username ) && !allowedUsersArray.includes( firstname ) )
+	{
+		await bot.sendMessage( chatId, `Sorry ${firstname}, you don't have permissions to download videos.` );
+		return;
+	}
+
+	if ( !fs.existsSync( "./downloads" ) )
+	{
+		fs.mkdirSync( "./downloads" );
+	}
+
+	if ( !fs.existsSync( userFolderPath ) )
+	{
+		fs.mkdirSync( userFolderPath );
+	}
+	// const result = await sendAndDeleteFiles( chatId, userFolderPath );
+
+	// if link include youtu.be or youtube.com
+	if ( link.includes( "youtu.be" ) || link.includes( "youtube.com" ) )
+	{
+		// If it's a YouTube link, use yt-dlp to download the highest quality video
+		const cmd = "yt-dlp -f b --write-description  --write-auto-sub --write-subs --embed-subs --write-info-json";
+		if ( exec( `cd ${userFolderPath}; ${cmd} "${link}"` ).code !== 0 )
+		{
+			await bot.sendMessage( chatId, "Error downloading YouTube video" );
+		}
+	}
+	else if ( link.includes( "instagram.com" ) )
+	{
+		const regex = /\/reels\/([^/]+)/;
+		const [ , match ] = link.match( regex );
+
+		const cmd = `instaloader -- -${match}`;
+		if ( exec( `cd ${userFolderPath}; ${cmd} "${link}"` ).code !== 0 )
+		{
+			await bot.sendMessage( chatId, "Error downloading instagram video" );
+		}
+	}
+	else
+	{
+		const cmd = `aria2c -x 15 "${link}"`;
+		if ( exec( `cd ${userFolderPath}; ${cmd} "${link}"` ).code !== 0 )
+		{
+			await bot.sendMessage( chatId, "Error downloading instagram video" );
+		}
+	}
+	await bot.sendMessage( chatId, "Did my best" );
+	await sendAndDeleteFiles( chatId, userFolderPath );
+
+});
+
+bot.onText( /\/y (.+)/, async ( msg, match ) =>
+{
+	const chatId = msg.chat.id;
+	const { username } = msg.from;
+	const firstname = msg.chat.first_name;
+	const [ , link ] = match;
+	const userFolder = username || firstname; // Use username if available, otherwise use firstname
+	const userFolderPath = `./downloads/${userFolder}/`;
+
+	if ( allowedUsersArray && !allowedUsersArray.includes( username ) && !allowedUsersArray.includes( firstname ) )
+	{
+		await bot.sendMessage( chatId, `Sorry ${firstname}, you don't have permissions to download YouTube videos.` );
+		return;
+	}
+
+	if ( !fs.existsSync( "./downloads" ) )
+	{
+		fs.mkdirSync( "./downloads" );
+	}
+
+	if ( !fs.existsSync( userFolderPath ) )
+	{
+		fs.mkdirSync( userFolderPath );
+	}
+
+	// Use yt-dlp to download the highest quality video
+	const cmd = "yt-dlp -f b";
+	if ( exec( `cd ${userFolderPath}; ${cmd} "${link}"` ).code !== 0 )
+	{
+		await bot.sendMessage( chatId, "Error downloading YouTube video" );
+	}
+	// await bot.sendMessage( chatId, "Did my best" );
+	await sendAndDeleteFiles( chatId, userFolderPath );
+});
---
+++ readme.md
@@ -0,0 +1,119 @@
+# LinkMagicBot 🚀
+
+`LinkMagicBot` is a Telegram bot that makes link downloading a breeze! This bot can grab videos and files from various platforms, including YouTube and Instagram.
+
+## Features 🌟
+
+- Download videos from YouTube 🎥
+- Download videos from Instagram 📸
+- Download any other file links 📦
+
+## How to Use 🤖
+
+1. Start a chat with `LinkMagicBot` on Telegram.
+2. Send a link you want to download using the `/d` command for videos or `/y` for YouTube videos.
+3. Sit back and relax as `LinkMagicBot` does the heavy lifting for you! 🪄
+4. The bot will send you the downloaded file.
+
+## Commands 📝
+
+- `/d [link]`: Download videos from various sources
+- `/y [link]`: Download YouTube videos
+
+## Permissions 🔐
+
+To use the bot, make sure you're an allowed user. The bot owner can grant permissions to specific users
+
+## Requirements 🛠️
+
+Make sure you have the following tools installed:
+
+- [yt-dlp](https://github.com/yt-dlp/yt-dlp) for downloading YouTube videos.
+- [instaloader](https://instaloader.github.io/) for downloading Instagram videos.
+- [aria2c](https://aria2.github.io/) for downloading other files.
+
+## Installation 🚚
+
+Clone this repository and install the required dependencies:
+
+```bash
+git clone https://github.com/mlibre/LinkMagicBot.git
+cd LinkMagicBot
+npm install
+```
+
+## Configuration ⚙️
+
+1. Set your Telegram bot token in the `process.env.TELEGRAM_BOT_TOKEN` environment variable.
+2. If needed, specify allowed users in the `process.env.ALLOWED_USERS` environment variable.
+
+You can create a `.env` file in the project directory with the following example content:
+
+```env
+TELEGRAM_BOT_TOKEN=YOUR_TELEGRAM_BOT_TOKEN_HERE
+ALLOWED_USERS=comma,separated,usernames,if,needed
+```
+
+Replace `YOUR_TELEGRAM_BOT_TOKEN_HERE` with your actual Telegram bot token. You can specify allowed users as a comma-separated list if needed.
+
+## Running the Bot 🏃
+
+To run the bot, execute the following command in your terminal:
+
+```bash
+node --env-file=.env bot.js
+```
+
+## Run using pm2
+
+To run the bot using pm2 process manager, install pm2 globally:
+
+```bash
+npm install pm2 -g
+pm2 startup
+systemctl enable pm2-root
+pm2 start bot.js --node-args="--env-file=.env"
+pm2 save
+pm2 logs 0
+```
+
+## Usage Examples 🚀
+
+### Download a Video (Non-YouTube)
+
+To download a video from a any sources, simply send the link using the `/d` command. For example:
+
+```bash
+/d https://example.com/video.mp4
+```
+
+### Download a YouTube Video
+
+To download a YouTube video, use the `/y` command followed by the YouTube video link. For example:
+
+```bash
+/y https://www.youtube.com/watch?v=YOUR_VIDEO_ID_HERE
+```
+
+## Local bot server
+
+Local bot server is a simple way to run the bot without using the Telegram bot API.  
+You can then upload files up to 2000mg
+
+```bash
+apt install cmake libssl-dev gperf
+git clone --recursive https://github.com/tdlib/telegram-bot-api.git
+cd telegram-bot-api
+mkdir build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+```
+
+## Contributing 🤝
+
+We welcome contributions from the community! Feel free to fork this repository, make improvements, and create pull requests.
+
+## License 📄
+
+This project is licensed under the MIT License.
+
+Happy downloading! 🚀📥
